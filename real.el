09dd38468e5398f0367f605cde80037e48e92d59
TripleRestriction <208179266+TripleRestriction@users.noreply.github.com>
2026-06-23 18:45:04 +0530

    separate file

---
+++ real.el
@@ -0,0 +1,59 @@
+(load-theme 'wombat t)
+(setq inhibit-startup-message t)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+(menu-bar-mode -1)
+(setq scroll-conservatively 101)
+(set-face-attribute 'default nil :family "JetBrains Mono Nerd Font" :height 120 :weight 'normal)
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode +1)
+(setq display-line-numbers-width-start t)
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("gnu" . "https://elpa.gnu.org/packages/")
+						  ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+(require 'use-package)
+(setq use-package-always-ensure t)
+(use-package counsel)
+(use-package ivy
+  :ensure t
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t
+        ivy-count-format "%d/%d "
+        ivy-initial-inputs-alist nil))
+(use-package evil)
+(evil-mode 1)
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
+(electric-pair-mode 1)
+(use-package corfu
+  :ensure t
+  :custom
+  (corfu-auto t)
+  (corfu-auto-delay 0)
+  (corfu-auto-prefix 1)
+  (corfu-cycle t)
+  :init
+  (global-corfu-mode))
+(add-hook 'c-mode-hook 'eglot-ensure)
+(add-hook 'c++-mode-hook 'eglot-ensure)
